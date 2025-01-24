1bffd792dc77f5ddab09edebfcdb1e5220c395f8
suraj <urusanonyms@gmail.com>
2025-01-25 02:06:13 +0545

    add express.json

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
 
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello, Backend World!');
+    res.send('Welcome to the backend');
 });
+
 
 
 app.listen(PORT, () => {
