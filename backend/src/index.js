73f30ce49dd9e8c00be1aadd4eb081ad96905540
suraj <urusanonyms@gmail.com>
2025-01-26 01:09:20 +0545

    failed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,34 +1,14 @@
-const express = require('express'); 
-const app = express(); 
-const PORT = 3000; 
-const path = require('path');
+const express = require('express')
+const path = require('path')
+const bcrypt = require('bcrypt')
+const port = 5000
 
 
+const app = express()
 
-// Middleware
-app.use('/static', express.static(path.join(__dirname, '../frontend')));
+app.set('view engine','ejs')
 
-app.get('/', (req, res) => {
-    const filePath = path.join(__dirname, '../frontend', 'loginpage', 'landing-page', 'main.html');
-    res.sendFile(filePath, (err) => {
-        if (err) {
-            console.error('Error sending file:', err);
-            res.status(500).send('An error occurred while loading the page.');
-        }
-    });
-});
 
-// Serve the register page
-app.get('/register', (req, res) => {
-    const filePath = path.join(__dirname, '../frontend', 'loginpage', 'index.html'); // Adjust this path
-    res.sendFile(filePath, (err) => {
-        if (err) {
-            console.error('Error loading register page:', err);
-            res.status(500).send('An error occurred while loading the register page.');
-        }
-    });
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(port,()=>{
+    console.log(`Server running on port: ${port}`)
+})
