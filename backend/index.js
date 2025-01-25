8d95b1bccd7f29e19775f848646d916be18fcb4a
suraj <urusanonyms@gmail.com>
2025-01-25 17:14:58 +0545

    small changes

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,13 @@ const express = require('express');
 const app = express(); 
 const PORT = 3000; 
 const path = require('path')
-
-// app.use(express.static(path.join(__dirname,'../frontend')))
-
 app.use(express.json())
 
 app.get('/', (req, res) => {
     const filePath = path.join(__dirname, '../frontend', 'loginpage', 'landing-page', 'main.html')
     res.sendFile(filePath);
-    
-});
 
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
